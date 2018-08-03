1cc9723669a534fd549e37bc083c380ba661eba5
Austen Wade <awade@merkleinc.com>
2018-08-03 17:05:05 -0600

    init commit

---
+++ ui.apps/src/main/content/jcr_root/apps/aem-vue/components/content/helloworld/webpack.module/helloworld.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="text">
+        Message: {{ message }}
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            message: String
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .text {
+        color: red;
+    }
+</style>
---
+++ ui.apps/src/main/content/jcr_root/apps/aem-vue/components/content/helloworld/webpack.module/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+// import HelloWorld from './helloworld.vue';
+
+new Vue({
+	el: '#helloworld',
+	data: {
+		message: "Hello"
+	}
+});
