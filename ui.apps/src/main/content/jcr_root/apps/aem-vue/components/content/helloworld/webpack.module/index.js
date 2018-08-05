311fc81aacf3d3caa934bed37fd0a9adefb26310
austen-wade <austen.wade.ut@gmail.com>
2018-08-04 18:46:36 -0600

    code cleanup

--- ui.apps/src/main/content/jcr_root/apps/aem-vue/components/content/helloworld/webpack.module/index.js
+++ ui.apps/src/main/content/jcr_root/apps/aem-vue/components/content/helloworld/webpack.module/index.js
@@ -2,14 +2,14 @@ import Vue from 'vue';
 import HelloWorldChild from './HelloWorldChild.vue';
 import { activateVue } from 'js/utils';
 
+// Connector Component for Single File Component
 Vue.component('hello-world', {
 	props: [ 'name' ],
 	components: {
 		'hello-world-child': HelloWorldChild
 	},
-	template: `<div>
-		<hello-world-child :message="name"></hello-world-child>
-	</div>`
+	template: `<hello-world-child :message="name"></hello-world-child>`
 })
 
+// Render Vue Components
 activateVue();
